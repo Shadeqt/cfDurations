4ab6d87e4ea9b199dcd27ef2f9a799b11ae0dea2
Shadeqt <itkofod@gmail.com>
2025-10-23 17:16:40 +0200

    Split into modules

---
+++ Modules/cfCompact.lua
@@ -0,0 +1,24 @@
+-- cfDurations Compact Module: Handles Party/Raid/Arena frame cooldowns
+
+local cfDurations = _G.cfDurations
+if not cfDurations then return end
+
+-- Update CompactUnitFrame buff
+local function UpdateCompactBuff(buffFrame, unit, index, filter)
+    local name, _, _, _, duration, expirationTime, caster, _, _, spellId = UnitBuff(unit, index, filter)
+    if name then
+        cfDurations.ApplyCooldown(buffFrame.cooldown, unit, spellId, caster, duration, expirationTime)
+    end
+end
+
+-- Update CompactUnitFrame debuff
+local function UpdateCompactDebuff(debuffFrame, unit, index, filter)
+    local name, _, _, _, duration, expirationTime, caster, _, _, spellId = UnitDebuff(unit, index, filter)
+    if name then
+        cfDurations.ApplyCooldown(debuffFrame.cooldown, unit, spellId, caster, duration, expirationTime)
+    end
+end
+
+-- Hook into Blizzard's CompactUnitFrame functions
+hooksecurefunc("CompactUnitFrame_UtilSetBuff", UpdateCompactBuff)
+hooksecurefunc("CompactUnitFrame_UtilSetDebuff", UpdateCompactDebuff)
---
+++ Modules/cfTarget.lua
@@ -0,0 +1,28 @@
+-- cfDurations Target Module: Handles TargetFrame cooldowns
+
+local cfDurations = _G.cfDurations
+if not cfDurations then return end
+
+-- Update TargetFrame buffs/debuffs
+local function UpdateTargetFrame(self)
+    if not self.unit then return end
+
+    -- Buffs
+    for i = 1, MAX_TARGET_BUFFS do
+        local name, _, _, _, duration, expirationTime, caster, _, _, spellId = UnitBuff(self.unit, i)
+        if not name then break end
+        local cooldown = _G[self:GetName() .. "Buff" .. i .. "Cooldown"]
+        cfDurations.ApplyCooldown(cooldown, self.unit, spellId, caster, duration, expirationTime)
+    end
+
+    -- Debuffs
+    for i = 1, MAX_TARGET_DEBUFFS do
+        local name, _, _, _, duration, expirationTime, caster, _, _, spellId = UnitDebuff(self.unit, i)
+        if not name then break end
+        local cooldown = _G[self:GetName() .. "Debuff" .. i .. "Cooldown"]
+        cfDurations.ApplyCooldown(cooldown, self.unit, spellId, caster, duration, expirationTime)
+    end
+end
+
+-- Hook into Blizzard's TargetFrame update function
+hooksecurefunc("TargetFrame_UpdateAuras", UpdateTargetFrame)
